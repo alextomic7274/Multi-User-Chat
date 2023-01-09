14ad28630ac354c4ab3c4d03a21c635779b4c50e
alex <alexfarkas57@gmail.com>
2023-01-09 02:42:03 +0000

    initial commit

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,2 +1,37 @@
-public class Client {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ChatClient {
+    private int portNumber = 0;
+    private String hostname = null;
+    private Scanner scanner = null;
+
+    public static void main(String[] args) {
+        try {
+            ChatClient chatClient = new ChatClient();
+            chatClient.startClient();
+        } catch (Exception e) {
+            System.out.println("Chat Client Failed");
+            e.printStackTrace();
+        }
+    }
+
+    public void startClient() throws UnknownHostException {
+        scanner = new Scanner(System.in);
+        System.out.println("Enter port: ");
+        portNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter hostname");
+        hostname = scanner.nextLine();
+        System.out.println("local"+ InetAddress.getLocalHost());
+
+        try (Socket socket = new Socket(InetAddress.getByName(hostname), portNumber)) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,15 +1,44 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.*;
 
-public class Server {
-    public void startServer() throws Exception {
-        ServerSocket s = new ServerSocket(1234);
+public class ChatServer {
 
-        while(true){
-            s.accept();
+    public static void main(String[] args) throws IOException {
+        int portNumber = Integer.parseInt(args[0]);
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            System.out.println("Waiting for clients on port "+portNumber);
+
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    System.out.println("Chat client connected from "+socket.getInetAddress()+" on port "+socket.getPort());
+                    Thread task = new ClientThread(socket);
+                    task.start();
+                } catch (IOException e) {
+                }
+            }
+        }   catch (IOException ex) {
+            System.out.println("Unable start chat server");
         }
 
     }
 
+    private static class ClientThread extends Thread {
+        private Socket socket;
+
+        ClientThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void m() {
+            super.run();
+        }
+    }
+
 
 }
+
+
+
+
